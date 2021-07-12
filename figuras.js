c970aee98c899d4b8112bb5454bc2142889a710b
jaBlancGuth <jablancr@gmail.com>
2021-07-11 21:02:14 -0600

    Adicion de inputs y botones para hacer la pagina de figuras interactiva, sin estilos y los resultados de los calculos se muestran con alert()

--- figuras.js
+++ figuras.js
@@ -72,3 +72,73 @@ console.log("el area del circulo es de "+areaCirculo+"cm^2")*/
 
 
 console.groupEnd();
+
+
+// Interaccion con html
+
+function calcularPerimetroCuadrado()
+{
+    const input = document.getElementById("input-cuadrado");
+    const value = input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert("el perimetro es "+perimetro)
+}
+
+
+function calcularAreaCuadrado()
+{
+    const input = document.getElementById("input-cuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert("el area es "+area)
+}
+
+
+function calcularPerimetroTriangulo()
+{
+    const inputl1 = document.getElementById("input-triangulo-l1");
+    const valuel1 = Number(inputl1.value);
+
+    const inputl2 = document.getElementById("input-triangulo-l2");
+    const valuel2 = Number(inputl2.value);
+
+    const inputb = document.getElementById("input-triangulo-b");
+    const valueb = Number(inputb.value);
+
+    const perimetro = perimetroTriangulo(valuel1,valuel2,valueb);
+    alert("el perimetro es "+perimetro)
+}
+
+function calcularAreaTriangulo()
+{
+    const inputb = document.getElementById("input-triangulo-b");
+    const valueb = Number(inputb.value);
+
+    const inputh = document.getElementById("input-triangulo-h");
+    const valueh = Number(inputh.value);
+    
+    const area = areaTriangulo(valueh,valueb);
+    alert("el area es "+area)
+}
+
+
+function calcularPerimetroCirculo()
+{
+    const inputr = document.getElementById("input-circulo");
+    const value = inputr.value;
+
+    const perimetro = circunferencia(value);
+    alert("la circunferencia es de "+perimetro)
+
+}
+
+
+function calcularAreaCirculo()
+{
+    const inputr = document.getElementById("input-circulo");
+    const value = inputr.value;
+
+    const area = areaCirculo(value);
+    alert("la circunferencia es de "+area)
+
+}
