3ba791610353454b8bc1f98e549204f16b1ba8c6
jaBlancGuth <jablancr@gmail.com>
2021-07-12 02:43:31 -0600

    Implementacion del reto de los cupones sin estilos

--- Taller2/descuentos.js
+++ Taller2/descuentos.js
@@ -4,3 +4,63 @@ function calcularDescuento(precio,descuento)
     const precioConDescuento= (precio*porcentajePrecioConDescuento)/100;
     return precioConDescuento;
 }
+
+function buttonCalculateDiscount()
+{
+    const inputPrice = document.getElementById("input-price");
+    const priceValue = inputPrice.value;
+    console.log(priceValue)
+
+    const inputDiscount = document.getElementById("input-discount");
+    const discountValue = inputDiscount.value;
+    console.log(discountValue)
+
+    const inputCoupon = document.getElementById("input-coupon");
+    const couponValue = inputCoupon.value;
+    console.log(couponValue)
+
+    if(couponValue===""){
+        const finalPrice = calcularDescuento(priceValue,discountValue);
+        console.log(finalPrice)
+        const pfinalPrice = document.getElementById("finalPriceP");
+        pfinalPrice.innerText = "El precio final de tu producto es de "+ finalPrice+ "$";
+    }
+    else{
+
+        const isCouponValueValid = function (coupon) {
+            return coupon.name === couponValue;
+        };
+    
+        const userCoupon = coupons.find(isCouponValueValid);
+    
+        if (!userCoupon) {
+            alert("El cupón " + couponValue + "no es válido");
+        }
+        else {
+            const descuento = userCoupon.discount;
+            const precioConDescuento = calcularDescuento(priceValue, descuento);
+    
+            const resultP = document.getElementById("finalPriceP");
+            resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+        }
+    
+    }
+
+}
+
+
+
+    const coupons = [
+        {
+            name: "JuanDC_es_Batman",
+            discount: 15,
+        },
+        {
+            name: "pero_no_le_digas_a_nadie",
+            discount: 30,
+        },
+        {
+            name: "es_un_secreto",
+            discount: 25,
+        },
+    ];
